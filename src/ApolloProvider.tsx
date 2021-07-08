ccacf06af8942de834ee357d92be685795d4e728
Kyle Getrost <kyle@motionhungry.com>
2021-07-09 04:27:11 +0700

    add apollo-link-retry

--- src/ApolloProvider.tsx
+++ src/ApolloProvider.tsx
@@ -1,21 +1,23 @@
-import { useAuth0, withAuthenticationRequired } from '@auth0/auth0-react';
+import { useAuth0, withAuthenticationRequired } from "@auth0/auth0-react";
 import {
-  ApolloClient,
   createHttpLink,
+  ApolloClient,
+  ApolloLink,
   ApolloProvider,
   InMemoryCache,
-} from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
-import { ReactNode } from 'react';
+} from "@apollo/client";
+import { RetryLink } from "@apollo/client/link/retry";
+import { setContext } from "@apollo/client/link/context";
+import { ReactNode } from "react";
 
 type Props = {
   children: ReactNode;
 };
 
 const Provider = ({ children }: Props) => {
-  const { isAuthenticated, getAccessTokenSilently } = useAuth0();
+  const { getAccessTokenSilently } = useAuth0();
 
-  const link = createHttpLink({
+  const httpLink = createHttpLink({
     uri: process.env.REACT_APP_GRAPHQL_URL,
   });
 
@@ -24,19 +26,19 @@ const Provider = ({ children }: Props) => {
     return {
       headers: {
         ...headers,
-        authorization: accessToken ? `Bearer ${accessToken}` : '',
+        authorization: accessToken ? `Bearer ${accessToken}` : "",
       },
     };
   });
 
+  const retryLink = new RetryLink();
+
   const client = new ApolloClient({
-    link: authLink.concat(link),
     cache: new InMemoryCache({}),
+    link: ApolloLink.from([authLink, retryLink, httpLink]),
   });
 
-  return isAuthenticated ? (
-    <ApolloProvider client={client}>{children}</ApolloProvider>
-  ) : null;
+  return <ApolloProvider client={client}>{children}</ApolloProvider>;
 };
 
 export default withAuthenticationRequired(Provider);
