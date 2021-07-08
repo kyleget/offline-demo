da79c255712d87c4997088bd5784815b89190142
Kyle Getrost <kyle@motionhungry.com>
2021-07-09 03:19:39 +0700

    debug

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -29,7 +29,7 @@ const Header = ({ isOffline, updateAvailable }: Props) => {
             {updateAvailable && <AppUpdateButton />}
           </Box>
           <Text textAlign="center" fontSize="2xl" fontWeight="semibold">
-            Messages
+            Messages 2.0
           </Text>
           <Box width="40px">
             <Menu />
